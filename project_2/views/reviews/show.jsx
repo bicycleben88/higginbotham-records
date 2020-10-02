f5629cef1ff399f51c70d6cc6d40159b60f81d59
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-10-01 20:49:45 -0400

    Reviews show page styled with flexbox

--- project_2/views/reviews/show.jsx
+++ project_2/views/reviews/show.jsx
@@ -6,18 +6,24 @@ class Show extends React.Component {
         const { reviews, id } = this.props
         return (
             <Layout>
-              {
-                  reviews.map((review) => {
-                      return <div>
-                                <p>{review.title}</p>
-                                <form action={`/reviews/${review._id}?_method=DELETE`} method="POST">
-                                    <input type="submit" defaultValue="Delete Review"/>
-                                </form>
-                                <a href={`/reviews/edit/${review._id}`}>Edit Review</a>
-                            </div>
-                  })
-              }
-              <a href={`/reviews/new/${id}`}>Add Review</a>
+                <h1>Reviews:</h1>
+                {
+                    reviews.map((review) => {
+                        return <div className="show-review-container">
+                                    <div>
+                                        <h3>Title: {review.title}</h3>
+                                        <p>Review: {review.body}</p>
+                                    </div>
+                                    <aside>
+                                        <form action={`/reviews/${review._id}?_method=DELETE`} method="POST">
+                                            <input type="submit" defaultValue="Delete Review"/>
+                                        </form>
+                                        <a href={`/reviews/edit/${review._id}`}><button> Edit Review</button></a>
+                                    </aside>
+                                </div>
+                            })
+                }
+              <a href={`/reviews/new/${id}`}><button>Add Review</button></a>
             </Layout>
         );
     }
--- project_2/views/user/show.jsx
+++ project_2/views/user/show.jsx
@@ -6,16 +6,22 @@ class Show extends React.Component {
         const { user } = this.props
         return (
             <Layout title={user.name}>
-                <h1>Welcome to the user page {user.name}</h1>
-                <a href="/records">home</a>
-                <img src={user.image} alt="profile picture"/>
-                <h3>Favorite Album:</h3>
-                <p>{user.favoriteAlbum}</p>
-                <a href="/auth/logout"><button>Log Out</button></a>
-                <a href={`edit/${user._id}`}><button>Update Profile</button></a>
-                <form action={`/user/${user._id}?_method=DELETE`} method="post">
-                    <input type="submit" defaultValue="Delete Profile"/>
-                </form>
+                <h1>Welcome to your page {user.name}!</h1>
+                <div className="show-user-container">
+                    <img src={user.image} alt="profile picture"/>
+                    <div>
+                        <h3>Favorite Album:</h3>
+                        <p>{user.favoriteAlbum}</p>
+                    </div>
+                    <aside>
+                        <a href="/records/new"><button>Add Album</button></a>
+                        <a href="/auth/logout"><button>Log Out</button></a>
+                        <a href={`edit/${user._id}`}><button>Update Profile</button></a>
+                        <form action={`/user/${user._id}?_method=DELETE`} method="post">
+                            <input type="submit" defaultValue="Delete Profile"/>
+                        </form>
+                    </aside>
+                </div>
             </Layout>
         );
     }
