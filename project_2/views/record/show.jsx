28811540d4345ec2c07301b21276e75a89c91f52
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-10-01 20:14:11 -0400

    Records show page styled with flexbox

--- project_2/views/record/show.jsx
+++ project_2/views/record/show.jsx
@@ -6,14 +6,22 @@ class Show extends React.Component {
         const { record } = this.props;
         return (
             <Layout>
-                    <img src={record.albumArtwork} alt="Album Artwork"/>
-                    <p>{record.albumTitle}</p>
-                    <p>{record.artist}</p>
-                    <a href={`edit/${record._id}`}><button>Edit Album</button></a>
-                    <form action={`/records/${record._id}?_method=DELETE`} method="POST">
-                        <input type="submit" defaultValue="Delete Record"/>
-                    </form>
-                    <a href={`/reviews/${record._id}`}>Reviews</a>
+                    <h1>{record.artist}</h1>
+                    <div className="show-records-container">
+                        <img src={record.albumArtwork} alt="Album Artwork" className="show-records-image"/>
+                        <ul>
+                            <li>{record.albumTitle}</li>
+                            <li>{record.genre}</li>
+                            <li>{record.releaseDate}</li>
+                        </ul>
+                        <aside>
+                            <a href={`edit/${record._id}`}><button>Edit Album</button></a>
+                            <form action={`/records/${record._id}?_method=DELETE`} method="POST">
+                                <input type="submit" defaultValue="Delete Record"/>
+                            </form>
+                            <a href={`/reviews/${record._id}`}> <button>  Reviews</button></a>
+                        </aside>
+                    </div>
             </Layout>
         );
     }
