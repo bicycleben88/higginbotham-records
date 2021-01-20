cb94c091e694d2a6baf5c1093809f359feb1539e
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-19 19:44:21 -0500

    bootstrap nav

--- project_2/views/auth/login.jsx
+++ project_2/views/auth/login.jsx
@@ -6,33 +6,33 @@ class LogIn extends React.Component {
     return (
       <Layout title="Log In">
         <form action="/auth/login" method="post" className="log-in-form">
-          <div className="mb-3">
-            <label for="username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              id="username"
-            />
+          <div className="row">
+            <div className="mb-3 col">
+              <label for="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                id="username"
+              />
+            </div>
+            <div className="mb-3 col">
+              <label for="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+              />
+            </div>
           </div>
-          <div className="mb-3">
-            <label for="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              id="password"
-            />
-          </div>
-          <div className="mb-3">
-            <button type="submit" value="login" className="btn btn-primary">
-              Log In
-            </button>
-          </div>
+          <button type="submit" value="login" className="btn btn-primary">
+            Log In
+          </button>
         </form>
       </Layout>
     );
--- project_2/views/auth/signup.jsx
+++ project_2/views/auth/signup.jsx
@@ -6,26 +6,33 @@ class SignUp extends React.Component {
     return (
       <Layout title="Sign Up">
         <form action="/auth/signup" method="POST">
-          <div className="mb-3">
-            <label for="name" className="form-label">
-              Name
-            </label>
-            <input type="text" className="form-control" name="name" id="name" />
-          </div>
-          <div className="mb-3">
-            <label for="favoriteAlbum" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="favoriteAlbum"
-              id="favoriteAlbum"
-            />
+          <div className="row">
+            <div className="mb-3 col">
+              <label for="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+              />
+            </div>
+            <div className="mb-3 col">
+              <label for="favoriteAlbum" className="form-label">
+                Favorite Album
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="favoriteAlbum"
+                id="favoriteAlbum"
+              />
+            </div>
           </div>
           <div className="mb-3">
             <label for="image" className="form-label">
-              Image
+              Image URL
             </label>
             <input
               type="text"
@@ -34,27 +41,29 @@ class SignUp extends React.Component {
               id="image"
             />
           </div>
-          <div className="mb-3">
-            <label for="username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              id="username"
-            />
-          </div>
-          <div className="mb-3">
-            <label for="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              id="Password"
-            />
+          <div className="row">
+            <div className="mb-3 col">
+              <label for="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                id="username"
+              />
+            </div>
+            <div className="mb-3 col">
+              <label for="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="Password"
+              />
+            </div>
           </div>
           <button type="submit" className="btn btn-primary">
             Create Profile
