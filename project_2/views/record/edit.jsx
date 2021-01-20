63edea5171635b3bdc6efd73bbc132c06ff99dd7
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-20 18:32:38 -0500

    bootstrap record add and update

--- project_2/views/record/edit.jsx
+++ project_2/views/record/edit.jsx
@@ -1,24 +1,88 @@
-const React = require('react');
-const Layout = require('../Layout');
+const React = require("react");
+const Layout = require("../Layout");
 
 class Edit extends React.Component {
-    render() {
-        const { record } = this.props;
-        return (
-            <Layout title={record.albumTitle}>
-                <div className="edit-album-container">
-                    <form action={`/records/edit/${record._id}?_method=PUT`} method="POST" className="edit-album-form">
-                        <input type="text" name="artist" defaultValue={record.artist}/>
-                        <input type="text" name="albumTitle" defaultValue={record.albumTitle}/>
-                        <input type="text" name="genre" defaultValue={record.genre}/>
-                        <input type="text" name="releaseDate" defaultValue={record.releaseDate}/>
-                        <input type="text" name="albumArtwork" defaultValue={record.albumArtwork}/>
-                        <input type="submit" defaultValue="Edit Album" className="button"/>
-                    </form> 
-                </div>
-            </Layout>
-        );
-    }
+  render() {
+    const { record } = this.props;
+    return (
+      <Layout title={record.albumTitle}>
+        <div className="container">
+          <form
+            action={`/records/edit/${record._id}?_method=PUT`}
+            method="POST"
+          >
+            <div className="row">
+              <div className="mb-3 col">
+                <label for="artist" className="form-label">
+                  Artist
+                </label>
+                <input
+                  type="text"
+                  name="artist"
+                  id="artist"
+                  className="form-control"
+                  defaultValue={record.artist}
+                />
+              </div>
+              <div className="mb-3 col">
+                <label for="albumTitle" className="form-label">
+                  ALbum Title
+                </label>
+                <input
+                  type="text"
+                  name="albumTitle"
+                  id="albumTitle"
+                  className="form-control"
+                  defaultValue={record.albumTitle}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="mb-3 col">
+                <label for="genre" className="form-label">
+                  Genre
+                </label>
+                <input
+                  type="text"
+                  name="genre"
+                  id="genre"
+                  className="form-control"
+                  defaultValue={record.genre}
+                />
+              </div>
+              <div className="mb-3 col">
+                <label for="releaseDate" className="form-label">
+                  Release Date
+                </label>
+                <input
+                  type="text"
+                  name="releaseDate"
+                  id="releaseDate"
+                  className="form-control"
+                  defaultValue={record.releaseDate}
+                />
+              </div>
+            </div>
+            <div className="mb-3 col">
+              <label for="albumArtwork" className="form-label">
+                Album Artwork
+              </label>
+              <input
+                type="text"
+                name="albumArtwork"
+                id="albumArtwork"
+                className="form-control"
+                defaultValue={record.albumArtwork}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Update Album
+            </button>
+          </form>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 module.exports = Edit;
--- project_2/views/record/new.jsx
+++ project_2/views/record/new.jsx
@@ -1,24 +1,79 @@
-const React = require('react');
-const Layout = require('../Layout');
+const React = require("react");
+const Layout = require("../Layout");
 
 class New extends React.Component {
-    render() {
-        const { record } = this.props;
-        return (
-            <Layout title="Add New Album">
-                <div className="new-album-container">
-                    <form action={`/records/new`} method="POST" className="new-album-form">
-                        <input type="text" name="artist" placeholder="Artist"/>
-                        <input type="text" name="albumTitle" placeholder="Album Title"/>
-                        <input type="text" name="genre" placeholder="Genre"/>
-                        <input type="text" name="releaseDate" placeholder="Release Date"/>
-                        <input type="text" name="albumArtwork" placeholder="Album Artwork URL"/>
-                        <input type="submit" defaultValue="Add Album" className="button"/>
-                    </form> 
-                </div>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout title="Add New Album">
+        <div className="container">
+          <form action={`/records/new`} method="POST">
+            <div className="row">
+              <div className="mb-3 col">
+                <label for="artist" className="form-label">
+                  Artist
+                </label>
+                <input
+                  type="text"
+                  name="artist"
+                  id="artist"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3 col">
+                <label for="albumTitle" className="form-label">
+                  ALbum Title
+                </label>
+                <input
+                  type="text"
+                  name="albumTitle"
+                  id="albumTitle"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="mb-3 col">
+                <label for="genre" className="form-label">
+                  Genre
+                </label>
+                <input
+                  type="text"
+                  name="genre"
+                  id="genre"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3 col">
+                <label for="releaseDate" className="form-label">
+                  Release Date
+                </label>
+                <input
+                  type="text"
+                  name="releaseDate"
+                  id="releaseDate"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="mb-3 col">
+              <label for="albumArtwork" className="form-label">
+                Album Artwork URL
+              </label>
+              <input
+                type="text"
+                name="albumArtwork"
+                id="albumArtwork"
+                className="form-control"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Add Album
+            </button>
+          </form>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 module.exports = New;
