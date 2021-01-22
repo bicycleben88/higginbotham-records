f492b76d966b59472c45d58d5b36ea20b93103cc
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-22 16:17:30 -0500

    add favicon

--- project_2/views/Layout.jsx
+++ project_2/views/Layout.jsx
@@ -6,6 +6,7 @@ class Layout extends React.Component {
       <html>
         <head>
           <script src={`${index}`} defer />
+          <link rel="icon" href="/music-note.svg" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
