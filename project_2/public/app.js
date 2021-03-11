205022c721bedd1587ad2e90a5331c35e4744406
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-10 22:19:44 -0500

    search results displayed --no styling

--- project_2/public/app.js
+++ project_2/public/app.js
@@ -9,6 +9,35 @@ async function getRecords() {
   await records.push(...data);
 }
 
+function findResults(searchTerm, records) {
+  return records.filter((record) => {
+    const regex = new RegExp(searchTerm, "gi");
+    return record.artist.match(regex) || record.albumTitle.match(regex);
+  });
+}
+
+function displayResults() {
+  const resultsArray = findResults(this.value, records);
+  const htmlText = resultsArray
+    .map((record) => {
+      if (this.value === "") return;
+      const regex = new RegExp(this.value, "gi");
+      const artist = record.artist.replace(
+        regex,
+        `<span> ${this.value} </span>`
+      );
+      const album = record.albumTitle.replace(
+        regex,
+        `<span> ${this.value} </span>`
+      );
+      return `
+        <li>${album} by ${artist}</li>
+      `;
+    })
+    .join("");
+  results.innerHTML = htmlText;
+}
+
 function playAudio(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const letter = document.querySelector(`.letter[data-key="${e.keyCode}"]`);
@@ -28,5 +57,7 @@ letters.forEach((letter) =>
   letter.addEventListener("transitionend", removeClass)
 );
 window.addEventListener("keydown", playAudio);
+searchBar.addEventListener("change", displayResults);
+searchBar.addEventListener("keyup", displayResults);
 
 getRecords();
